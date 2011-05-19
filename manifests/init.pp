5c210840ea9aad65b3404c19fa67531c8a756a46
Eivind Uggedal <eivind@uggedal.com>
2011-05-19 13:00:20 +0200

    Logging, logration, seperate state directory. Using scripts from wheezy package.

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,17 +5,30 @@ class monit($ensure=present, $admin="", $interval=60) {
     ensure => $ensure,
   }
 
-  file { "/etc/monit/monitrc":
-    ensure => $ensure,
-    content => template("monit/monitrc.erb"),
-    mode => 600,
-    require => Package["monit"],
-  }
+  file {
+    "/etc/monit/monitrc":
+      ensure => $ensure,
+      content => template("monit/monitrc.erb"),
+      mode => 600,
+      require => Package["monit"];
 
-  file { "/etc/default/monit":
-    ensure => $ensure,
-    content => "startup=1\n",
-    require => Package["monit"],
+    "/etc/default/monit":
+      ensure => $ensure,
+      content => "startup=1\n",
+      require => Package["monit"];
+
+    "/etc/logrotate.d/monit":
+      ensure => $ensure,
+      source => "puppet:///modules/monit/monit.logrotate",
+      require => Package[monit];
+
+    "/var/lib/monit/events":
+      ensure => $ensure ? {
+        'present' => "directory",
+        default => $ensure,
+      },
+      mode => 700,
+      force => $is_absent;
   }
 
   service { "monit":
@@ -24,7 +37,8 @@ class monit($ensure=present, $admin="", $interval=60) {
     hasrestart => true,
     pattern => "/usr/sbin/monit",
     subscribe => File["/etc/monit/monitrc"],
-    require => [File["/etc/nginx/nginx.conf"],
-                File["/etc/logrotate.d/nginx"]],
+    require => [File["/etc/monit/monitrc"],
+                File["/etc/logrotate.d/monit"],
+                File["/var/lib/monit/events"]],
   }
 }
