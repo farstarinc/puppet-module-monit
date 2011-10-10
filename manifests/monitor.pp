3d9843c9a5eb3e212b7aa1623ebe43a3862eb45b
Ben Davis <bendavis78@gmail.com>
2011-10-09 19:28:59 -0500

    added upstart support

--- manifests/monitor.pp
+++ manifests/monitor.pp
@@ -2,7 +2,14 @@ define monit::monitor($pidfile,
                       $ensure=present,
                       $ip_port=0,
                       $socket="",
-                      $checks=[]) {
+                      $checks=[],
+                      $upstart=false) {
+  
+  $upstart_name = $upstart ? {
+    false => undef,
+    true => $name,
+    default => $upstart,
+  }
 
   file { "/etc/monit/conf.d/$name.conf":
     ensure => $ensure,
