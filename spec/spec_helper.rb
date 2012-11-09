a2267fbbacbaf1dc47a7b7020b7213996efcad01
Jonathan Thurman <jthurman@newrelic.com>
2012-11-09 15:57:26 -0800

    Update spec tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,1 @@
-require 'puppet'
-require 'rspec'
-require 'rspec-puppet'
-
-def param_value(subject, type, title, param)
-  subject.resource(type, title).send(:parameters)[param.to_sym]
-end
-
-RSpec.configure do |c|
-  c.module_path  = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures/modules'))
-  c.manifest_dir = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures/manifests'))
-end
+require 'puppetlabs_spec_helper/module_spec_helper'
